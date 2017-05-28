d5f8636cd08b6b3931a0edc9a2c2c8addf65faf1
python set <jisoo@bagjisus-MacBook-Pro.local>
2017-05-28 17:23:48 +0900

    add the neural1

--- neural1.py
+++ neural1.py
@@ -1,11 +1,12 @@
- import numpy as np
+import numpy as np
 
- X =  np.array ([1,2])
- X.shape
 
- W = np.array([1,3,5],[2,4,6])
- print(W)
- W.shape
+X =  np.array ([1,2])
+print(X.shape)
 
- Y = np.dot(X,W)
- print(Y)
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+print(W.shape)
+
+Y = np.dot(X,W)
+print(Y)
