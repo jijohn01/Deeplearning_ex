3c7184db90067705d7350a400a3c6e6131b9d2b3
python set <jisoo@bagjisus-MacBook-Pro.local>
2017-05-28 22:46:24 +0900

    Add sigmoid Neural 구현 예시

--- neural1.py
+++ neural1.py
@@ -1,12 +1,31 @@
 import numpy as np
 
+def sigmoid(x):#활성화 함수
+	return 1 / (1 + np.exp(-x))
 
-X =  np.array ([1,2])
-print(X.shape)
+X = np.array([1.0 , 0.5])
+W1 = np.array([[0.1, 0.3, 0.5],[0.2 , 0.4 , 0.6]])
+B1 = np.array([0.1 , 0.2, 0.3])
 
-W = np.array([[1, 3, 5], [2, 4, 6]])
-print(W)
-print(W.shape)
+A1 = np.dot(X,W1) + B1
+#입력과 가중치를 곱하고 편향을 더함
 
-Y = np.dot(X,W)
+Z1 = sigmoid(A1)
+print(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4],[0.2, 0.5],[0.3,0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1,W2) + B2
+Z2 = sigmoid(A2)
+print(A2)
+print(Z2)
+#은닉층 2
+
+W3 = np.array([[0.1 , 0.2],[0.4, 0.2]])
+B3 = np.array([0.2,0.1])
+
+A3 = np.dot(Z2,W3) + B3
+Y = A3 #sigma ()
 print(Y)
